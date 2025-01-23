fd98017671811b3a35b4a4d9b6909034064a19f8
408854 <408854@niuitmo.ru>
2025-01-23 18:16:05 +0300

    Added custom barebones terrain generation and continentalness

--- src/main/java/fogy/mixin/NoiseBasedChunkGeneratorMixin.java
+++ src/main/java/fogy/mixin/NoiseBasedChunkGeneratorMixin.java
@@ -14,13 +14,13 @@ public class NoiseBasedChunkGeneratorMixin {
 
 	@Overwrite
 	private static Aquifer.FluidPicker createFluidPicker(NoiseGeneratorSettings pSettings) {
-		Aquifer.FluidStatus aquifer$fluidstatus = new Aquifer.FluidStatus(-100, Blocks.LAVA.defaultBlockState());
+		Aquifer.FluidStatus aquifer$fluidstatus = new Aquifer.FluidStatus(-480, Blocks.LAVA.defaultBlockState());
 		int i = pSettings.seaLevel();
 		Aquifer.FluidStatus aquifer$fluidstatus1 = new Aquifer.FluidStatus(i, pSettings.defaultFluid());
 		Aquifer.FluidStatus aquifer$fluidstatus2 = new Aquifer.FluidStatus(DimensionType.MIN_Y * 2,
 				Blocks.AIR.defaultBlockState());
 		return (p_224274_, p_224275_, p_224276_) -> {
-			return p_224275_ < Math.min(-100, i) ? aquifer$fluidstatus : aquifer$fluidstatus1;
+			return p_224275_ < Math.min(-480, i) ? aquifer$fluidstatus : aquifer$fluidstatus1;
 		};
 	}
 
